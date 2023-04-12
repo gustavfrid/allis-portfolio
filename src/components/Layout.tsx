34c17f1179d71b9a43fdeb86093ead9d6e4d8eea
Gustav <gustav.frid@gmail.com>
2023-04-12 21:02:33 +0200

    sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,19 @@
 import { Navbar } from './Navbar'
 import { Sidebar } from './Sidebar'
 import styles from '@/styles/Layout.module.css'
+import bgImg from '@/images/background.jpg'
 
 import { PropsWithChildren } from 'react'
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <div>
+      <div className={styles.background} />
       <Navbar />
       <div className={styles.main}>
         <Sidebar />
-
         <div className={styles.not_sidebar}>{children}</div>
       </div>
-    </>
+    </div>
   )
 }
