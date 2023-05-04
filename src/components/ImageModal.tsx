19ba153b15f8ba4340518f9aa1a2ecc358792aff
Gustav <gustav.frid@gmail.com>
2023-05-04 13:33:07 +0200

    slideshow

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -19,18 +19,23 @@ export const ImageModal = ({
   return (
     <div className={styles.container}>
       <div className={styles.button_container}>
-        {prevImgSrc && prevImgSrc !== 'null' && (
-          <button className={styles.button} onClick={() => handleImageClick(prevImgSrc)}>
-            {'<'}
-          </button>
-        )}
-        <button className={styles.button_close} onClick={onClose}></button>
-
-        {nextImgSrc && nextImgSrc !== 'null' && (
-          <button className={styles.button} onClick={() => handleImageClick(nextImgSrc)}>
-            {'>'}
-          </button>
-        )}
+        <button
+          className={styles.button}
+          style={{ visibility: `${prevImgSrc === 'null' ? 'hidden' : 'visible'}` }}
+          disabled={prevImgSrc === 'null'}
+          onClick={() => handleImageClick(prevImgSrc ? prevImgSrc : 'null')}>
+          {'<'}
+        </button>
+        <button className={styles.button_close} onClick={onClose}>
+          x
+        </button>
+        <button
+          className={styles.button}
+          style={{ visibility: `${nextImgSrc === 'null' ? 'hidden' : 'visible'}` }}
+          disabled={nextImgSrc === 'null'}
+          onClick={() => handleImageClick(nextImgSrc ? nextImgSrc : 'null')}>
+          {'>'}
+        </button>
       </div>
       <Image src={`/${imgSrc}`} alt='Hero image' fill style={{ objectFit: 'contain' }} />
     </div>
